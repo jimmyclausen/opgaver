56b588196aea94e069162130ee64cc25c5fcad8b
jimju <jimju@10.140.81.84>
2012-11-30 11:34:39 +0100

    Math

---
+++ Math/src/Tal_fra_til.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 15, 2011
+
+@author: ubuntu
+'''
+# ex 2.1
+print( 5*10 )#printer 50
+
+
+#ex 2.2
+for i in range(6,25):
+    print("%x\t"%i)#printer fra 6-25 i hex
+    
+# ex 2.3
+for i in range(9,16):#printer fra 9-16 i dec, som kun er til 15 da den starter fra 0
+    print("%d"%i)
---
+++ Math/src/bin_hex_oct_omregner.py
@@ -0,0 +1,22 @@
+'''
+Created on 29/11/2012
+
+@author: jimju
+'''
+
+
+print ("hallo binary world")
+
+y= 15 
+
+b = bin(y)
+
+x = hex(y)
+
+o = oct(y)
+
+
+print("decimal=",y, "bin=",b, "hex=",x,"Octal=",o)
+
+print("end of program")
+
